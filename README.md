89f016ac94266615402a848f0316cfa89c199bec
lih <hao.li@edu.esiee.fr>
2022-11-27 18:24:06 +0100

    1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# FULL-STACK_POKEMON
+# FastAPI和Vue，前端是做测试的，用的别人的模板，能改改就改改
+
+
+
+## 如何部署项目-Docker
+
+1. Build images 和 建containers:
+
+```sh
+$ docker-compose up -d --build
+```
+
+2. 应用迁移（基本不会用到，但是人家模板里面加了。我试了试，加不加都一样）:
+
+```sh
+$ docker-compose exec backend aerich upgrade
+```
+
+然后看后台以下网址： [http://localhost:5000/docs](http://localhost:5000/docs)
+
+
+前端网址 [http://localhost:8080](http://localhost:8080) 
---
+++ services/frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <p>FastAPI + Vue(Pokemon).</p>
+
+    <div v-if="isLoggedIn" id="logout">
+      <p id="logout">Click <a href="/dashboard">here</a> to view all pokemons.</p>
+    </div>
+    <p v-else>
+      <span><a href="/register">Register</a></span>
+      <span> or </span>
+      <span><a href="/login">Log In</a></span>
+    </p>
+  </section>
+</template>
+<script>
+
+export default {
+  name: 'Home',
+  computed : {
+    isLoggedIn: function() {
+      return this.$store.getters.isAuthenticated;
+    }
+  },
+}
+</script>
